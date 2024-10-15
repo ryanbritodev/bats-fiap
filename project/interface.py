b4aaaf9adc1034c156cba87d4d657090d0d114ac
Ryan Brito <rm554497@fiap.com.br>
2024-10-14 21:18:32 -0300

    Ícone, Botões e Função Centralizar

--- project/interface.py
+++ project/interface.py
@@ -1,55 +1,88 @@
+# INTERFACE GRÁFICA AUTOBATS
+# IMPORTANTE: se você achou este pen drive, NÃO EXECUTE OS COMANDOS, eles servem
+# única e exclusivamente para auxílio de monitores, utilizá-los indevidamente pode acarretar
+# em problemas sérios.
+# Créditos:
+# Victor Flávio Demarchi Viana
+# Ryan Brito Pereira Ramos
+
 import os
-import tkinter as tk
 from tkinter import Canvas
 from tkinter import PhotoImage
+import customtkinter
+
+customtkinter.set_appearance_mode("dark")
+customtkinter.set_default_color_theme("blue")
+
+
+def center(win):
+    """
+    --> Função que centraliza uma janela do Tkinter
+    :param win: A janela principal que você deseja centralizar
+    """
+    win.update_idletasks()
+    width = win.winfo_width()
+    frm_width = win.winfo_rootx() - win.winfo_x()
+    win_width = width + 2 * frm_width
+    height = win.winfo_height()
+    titlebar_height = win.winfo_rooty() - win.winfo_y()
+    win_height = height + titlebar_height + frm_width
+    x = win.winfo_screenwidth() // 2 - win_width // 2
+    y = win.winfo_screenheight() // 2 - win_height // 2
+    win.geometry('{}x{}+{}+{}'.format(width, height, x, y))
+    win.deiconify()
 
 
 def executar_comando_desligar():
-    os.system("shutdown.bat")  # Substitua pelo caminho do seu arquivo .bat
+    os.system("shutdown.bat")  # Substituir posteriormente pelos caminhos dos bats
 
-def executar_comando_ligar():
-    os.system("ligar.bat")  # Substitua pelo caminho do seu arquivo .bat
 
-def executar_comando_logar():
-    os.system("logar.bat")  # Substitua pelo caminho do seu arquivo .bat
+# Login
+usuario_nome = "Ryan"  # Pensei de fazermos uma tela de login, pra primeira vez que o usuário entrar, onde ele vai definir o nome, a senha pro cmd e a senha e o usuário do runas
 
-# Criando a janela principal
-janela = tk.Tk()
+# Janela principal
+janela = customtkinter.CTk()
+
+janela.iconbitmap("./assets/fiap-ico.ico")
 
 # Tamanho da Janela
-janela.geometry("600x600")
+janela.geometry("626x391")
+janela.minsize(626, 391)  # Garantir tamanho mínimo da janela
 
 # Título
 janela.title("FIAP Autobats")
 
-# Criando Canvas
+# Centralizar a janela
+center(janela)
+
+# Canvas
 canvas1 = Canvas(janela, width=600, height=600)
 canvas1.pack(fill="both", expand=True)
 
 # Carregar e exibir imagem de fundo
-imagemFundo = PhotoImage(file="fundoFiap.png")
+imagemFundo = PhotoImage(file="./assets/fundoFiap.png")
 canvas1.create_image(0, 0, image=imagemFundo, anchor="nw")
 
-# Carregar o logo da FIAP e posicioná-lo
-logoFiap = PhotoImage(file="fiapLogo.png")
-canvas1.create_image(300, 100, image=logoFiap)
+# Logo da FIAP
+logoFiap = PhotoImage(file="./assets/fiapLogo.png")
+canvas1.create_image(400, 100, image=logoFiap)
 
 # Cores dos Botões
-cor_fundo = "#e4336c"  # Cor de fundo
-cor_texto = "white"  # Cor do texto
+cor_fundo = "#e4336c"
+cor_fundo_escuro = "#c62c5c"
 
 # Fonte
-fonte = ("", 12, "bold")
+fonte = ("Arial", 18, "bold")
 
-# Criando botões no Canvas
-botao_desligar = tk.Button(janela, text="Desligar", command=executar_comando_desligar, width=15, bg=cor_fundo, fg=cor_texto, font=fonte)
-botao_ligar = tk.Button(janela, text="Ligar", command=executar_comando_ligar, width=15,  bg=cor_fundo, fg=cor_texto, font=fonte)
-botao_logar = tk.Button(janela, text="Logar", command=executar_comando_logar, width=15,  bg=cor_fundo, fg=cor_texto, font=fonte)
+# Mensagem de boas-vindas personalizada
+canvas1.create_text(355, 165, text=f"Bem-vindo,", fill="white", font=("Arial", 20, "bold"))
+canvas1.create_text(475, 165, text=f"{usuario_nome}!", fill=cor_fundo, font=("Arial", 20, "bold"))
+
+# Botões no Canvas (sem command, por enquanto)
+botao_desligar = customtkinter.CTkButton(janela, text="Desligar Lab", width=170, height=40, font=fonte, fg_color=cor_fundo, hover_color=cor_fundo_escuro)
 
 # Posicionando os botões sobre o Canvas
-canvas1.create_window(300, 300, window=botao_desligar)  # Posição central no Canvas
-canvas1.create_window(300, 350, window=botao_ligar)     # Ajuste a posição de acordo
-canvas1.create_window(300, 400, window=botao_logar)
+canvas1.create_window(400, 350, window=botao_desligar)
 
 # Iniciando o loop da interface
 janela.mainloop()
